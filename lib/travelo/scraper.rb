306ebb4abf4d976e32ad7047840f66b6bf5be61e
Adi Skop <adiskop@gmail.com>
2020-09-11 15:25:47 +0000

    missing class name

--- lib/travelo/scraper.rb
+++ lib/travelo/scraper.rb
@@ -2,7 +2,7 @@
 
   class Travelo::Scraper
 
-    def self.
+    def self.scrape_destinations
         index_page = Nokogiri::HTML(open("https://www.townandcountrymag.com/leisure/travel-guide/a25052734/best-places-to-travel-2019/"))
         binding.pry
     end
