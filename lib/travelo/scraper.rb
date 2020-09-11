00d5b89e49822faaefa4f5aece609b7c955cb37e
Adi Skop <adiskop@gmail.com>
2020-09-11 15:23:30 +0000

    changing website again

--- lib/travelo/scraper.rb
+++ lib/travelo/scraper.rb
@@ -2,8 +2,8 @@
 
   class Travelo::Scraper
 
-    def self.scrape_destinations
-        index_page = Nokogiri::HTML(open("https://www.lonelyplanet.com/best-in-travel/countries"))
+    def self.
+        index_page = Nokogiri::HTML(open("https://www.townandcountrymag.com/leisure/travel-guide/a25052734/best-places-to-travel-2019/"))
         binding.pry
     end
 
