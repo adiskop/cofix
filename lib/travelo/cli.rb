fb4d45adf118b90ead3733d12af229dff4dd0563
Adi Skop <adiskop@gmail.com>
2020-09-12 12:25:39 +0000

    adding setup to 2nd scrape

--- lib/travelo/cli.rb
+++ lib/travelo/cli.rb
@@ -9,7 +9,8 @@ class Travelo::CLI
     #scrape the destinations - call to the scraper class
     #list top 10 destinations
     list_destinations
-    get_destination_method
+    puts "Please select city you want to explore it's coffee shops, by choosing a number 1-10: "
+    get_destination_method    #asked for input and reported the title of destination
     #ask for input
     #call another method
   end
@@ -25,9 +26,36 @@ class Travelo::CLI
   end
 
   def get_destination_method
-    puts "Please select city you want to explore it's coffee shops, by choosing a number 1-10: "
-    input = gets.strip
+    input = gets.strip  #"A private war"  "exit"
+    index = input.to_i - 1
+      if index.between?(0,9)    #a string will be -1
+      destination = @sorted_destinations[index]
+      puts "#{destination.title}:"
+        #continue with our program
+        #find that destination
+        #2nd scrape
+        #list destination that corresponds to this number
+        want_more_info(destination)
+      elsif input == "exit"
+        #allow this method to end
+      else
+        puts "Sorry! invalid input"
+        get_destination_method    #recursion(calling the same method from inside the method)
+      end
+  end
 
+  def want_more_info(destination)
+    puts "Read more?"
+    input = nil
+    until input == "Y" || input == "N"
+      puts "type Y or N"
+      input = gets.strip.upcase
+    end
+    if input == "y"
+      #2nd scrape
+    else
+    puts "you ended"
+    end
   end
 
   def list_review
