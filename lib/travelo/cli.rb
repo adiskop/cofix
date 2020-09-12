1281650cc60bce10edc705a6e2c043e291e6e1d8
Adi Skop <adiskop@gmail.com>
2020-09-12 10:59:43 +0000

    adding methods

--- lib/travelo/cli.rb
+++ lib/travelo/cli.rb
@@ -1,26 +1,39 @@
 class Travelo::CLI
+  attr_accessor :sorted_destinations
 
   def start
-    puts "Welcome to Travelo!"
-    puts "Here are the top 50 destinations:"
+    puts "Welcome to Travelo! The app that takes you to a coffee journey in 10 US metropolises"
+    puts "Where would you like to discover coffee shops today?"
     Travelo::Scraper.scrape_destinations
+    sort_destinations
     #scrape the destinations - call to the scraper class
-    #list top 50 destinations
+    #list top 10 destinations
     list_destinations
+    get_destination_method
     #ask for input
     #call another method
   end
 
-def list_destinations
-  Travelo::Destination.all.each.with_index(1) do |destination, index|
-    puts "#{index}. #{destination.title}"
+  def sort_destinations
+    @sorted_destinations = Travelo::Destination.all.sort_by{|destination| destination.title}
   end
-end
 
-def list_review
-  #if movie details have NOT been scraped,
-  #scrape
-end
+  def list_destinations
+    @sorted_destinations.each.with_index(1) do |destination, index|
+    puts "#{index}. #{destination.title}"
+      end
+  end
+
+  def get_destination_method
+    puts "Please select city you want to explore it's coffee shops, by choosing a number 1-10: "
+    input = gets.strip
+
+  end
+
+  def list_review
+    #if movie details have NOT been scraped,
+    #scrape
+  end
 
 
 end
--- lib/travelo/scraper.rb
+++ lib/travelo/scraper.rb
@@ -6,13 +6,13 @@ class Travelo::Scraper
         array_of_destinations = index_page.css("div.col-12.col-md-6")
 
         array_of_destinations.each do |destination_card|
-    attributes =  {
+          attributes =  {
         title: destination_card.css("h3.entry-title.h4.sm-h2.m-h1 a").text ,
         url:  destination_card.css("h3.entry-title.h4.sm-h2.m-h1 a")[0].attributes['href'].value,
         read_time: destination_card.css("div.entry-meta.entry-meta-small").text ,
       }
       destination = Travelo::Destination.new(attributes)
-    
+
     end
   end
 
